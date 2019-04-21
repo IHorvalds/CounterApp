f9f385413fdf15ae45456a5d192838c537a1c19e
Tudor Croitoru <dominiccroitoru@gmail.com>
2019-04-21 14:20:36 +0300

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Tudor Croitoru on 21/04/2019.
+//  Copyright © 2019 Tudor Croitoru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
